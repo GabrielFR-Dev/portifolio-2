a253d90b8b25e0b3dcc8a075194ef7c1f6a0673d
Gabriel Ferreira <164042709+GabrielFR-Dev@users.noreply.github.com>
2026-01-22 23:23:26 -0300

    menu mobile editado

--- portifolio/src/componentes/menu/menuMobile/index.jsx
+++ portifolio/src/componentes/menu/menuMobile/index.jsx
@@ -1,32 +1,36 @@
 "use client"
 import estilos from "./menuMobile.module.css";
 import Link from "next/link";
-export default function MenuMobile({fecharMenu}) {
+export default function MenuMobile({ fecharMenu }) {
     return (
-        <main className={estilos.menuMobile}>
-            <ul>
-                <li>
-                    <Link href="/" className={estilos.linkMobile} aria-label="Ir para página principal" rel="noopener noreferrer" onClick={fecharMenu}>
-                        About
-                    </Link>
-                </li>
+        <div className={estilos.overlay}>
+            <nav className={estilos.menuMobile}>
+                <ul>
+                    <li>
+                        <Link href="/" className={estilos.linkMobile} aria-label="Ir para página principal" rel="noopener noreferrer" onClick={fecharMenu}>
+                            About
+                        </Link>
+                    </li>
 
-                <li>
-                    <Link href="/habilidades" className={estilos.linkMobile} aria-label="Ir para minhas Habilidades" rel="noopener noreferrer" onClick={fecharMenu}>
-                        My Skills
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/projetos" className={estilos.linkMobile} aria-label="Ir para meus projetos" rel="noopener noreferrer" onClick={fecharMenu}>
-                        Projects
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/formacao" className={estilos.linkMobile} aria-label="Ir para minha formação" rel="noopener noreferrer" onClick={fecharMenu}>
-                        Training
-                    </Link>
-                </li>
-            </ul>
-        </main>
+                    <li>
+                        <Link href="/habilidades" className={estilos.linkMobile} aria-label="Ir para minhas Habilidades" rel="noopener noreferrer" onClick={fecharMenu}>
+                            My Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/projetos" className={estilos.linkMobile} aria-label="Ir para meus projetos" rel="noopener noreferrer" onClick={fecharMenu}>
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/formacao" className={estilos.linkMobile} aria-label="Ir para minha formação" rel="noopener noreferrer" onClick={fecharMenu}>
+                            Training
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
     )
 }
